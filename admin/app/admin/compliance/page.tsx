1dcac5d77db3f7d8303cf1a57faad6f5d8ead24f
profyk <profykersha18@gmail.com>
2026-05-24 16:05:08 +0200

    Update page.tsx

--- admin/app/admin/compliance/page.tsx
+++ admin/app/admin/compliance/page.tsx
@@ -193,4 +193,4 @@ export default function CompliancePage() {
       />
     </AdminShell>
   );
-}                                                                                                    }
+ }                                                                                                    }
